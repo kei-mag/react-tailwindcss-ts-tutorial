10cd68ba2ee6b0b6394bc59e538d97b25578c6f0
kei-mag <61485115+kei-mag@users.noreply.github.com>
2025-10-17 01:57:25 +0900

    45. コンポーネントの使い方を知る

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import { StrictMode } from "react"; // より警告を表示して厳密なルールのもと開発を行う
 import { createRoot } from 'react-dom/client';
+import { App } from "./app"; // App.jsからApp関数をimport
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-
-const App = () => {
-    return (
-    <>
-    <h1>こんにちは!</h1>
-    <p>お元気ですか?</p>
-    </>
-    )
-};
 
 // App関数のコンポーネントをAppタグとして書く。
 root.render(<StrictMode>
