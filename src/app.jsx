cfc384732650c66134c0fd1df2bb902a7ac233a1
kei-mag <61485115+kei-mag@users.noreply.github.com>
2025-10-17 02:03:37 +0900

    46. Reactでのイベントやスタイルの扱い方を知る

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,16 @@
 // exportタグを書いて他ファイルから参照可能にする
 export const App = () => {
+    const onClickButton = () => alert();
+    const contentStyle = {
+        color: "blue",
+        fontSize: "18px",
+        margin: 100
+    }
     return (
     <>
-    <h1>こんにちは!</h1>
-    <p>お元気ですか?</p>
+    <h1 style={ { color: "red" } }>こんにちは!</h1>
+    <p style={contentStyle}>お元気ですか?</p>
+    <button onclick={onClickButton}>ボタン</button>
     </>
     )
 };
