c4737c1726b64d59726b5f8402d1e7b272917374
kei-mag <61485115+kei-mag@users.noreply.github.com>
2025-10-17 11:27:57 +0900

    54. JSXで構造を作成

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,6 +2,41 @@ import "./styles.css";
 
 export const Todo = () => {
     return (
-        <div>hoge</div>
-    )
+        <>
+        <div>
+            <input placeholder="TODOを入力" />
+            <button>追加</button>
+        </div>
+        <div>
+            <p>未完了のTODO</p>
+            <ul>
+                <li>
+                    <p>TODOです</p>
+                    <button>完了</button>
+                    <button>削除</button>
+                </li>
+                <li>
+                    <p>TODOです</p>
+                    <button>完了</button>
+                    <button>削除</button>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <p>完了のTODO</p>
+            <ul>
+                <li>
+                    <p>TODOでした</p>
+                    <button>完了</button>
+                    <button>削除</button>
+                </li>
+                <li>
+                    <p>TODOでした</p>
+                    <button>完了</button>
+                    <button>削除</button>
+                </li>
+            </ul>
+        </div>
+        </>
+    );
 }
