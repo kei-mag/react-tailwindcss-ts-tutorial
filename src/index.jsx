5086bbd89bbcee1046f30f693f8dfc12b50644b4
kei-mag <61485115+kei-mag@users.noreply.github.com>
2025-10-17 11:15:04 +0900

    53. TODOアプリ作成の準備

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react"; // より警告を表示して厳密なルールのもと開発を行う
 import { createRoot } from 'react-dom/client';
-import { App } from "./App"; // App.jsからApp関数をimport
+import { Todo } from "./Todo"; // Todo.jsからTodo関数をimport
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-// App関数のコンポーネントをAppタグとして書く。
+// Todo関数のコンポーネントをTodoタグとして書く。
 root.render(<StrictMode>
-    <App />
+    <Todo />
     </StrictMode>
 );
